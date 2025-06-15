396ee5a8e7e6d21a20be2abc1b37467d7d21fda6
Kumar Piyush <kumarpiyushxd@gmail.com>
2025-06-15 17:28:21 +0530

    minor changes

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -2,6 +2,7 @@ export default {
   providers: [
     {
       domain: process.env.NEXT_PUBLIC_CLERK_FRONTEND_API_URL,
+      issuerDomain: process.env.CLERK_JWT_ISSUER_DOMAIN,
       applicationID: "convex",
     },
   ]
